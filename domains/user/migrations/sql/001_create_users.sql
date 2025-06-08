c8ca75aa895506d0ddcc6f127f639f25aac59914
sengokudaikon <50545866+sengokudaikon@users.noreply.github.com>
2025-06-08 20:54:02 +0200

    fix sql

--- domains/user/migrations/sql/001_create_users.sql
+++ domains/user/migrations/sql/001_create_users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    id         UUID PRIMARY KEY             DEFAULT uuidv7(),
+    id         UUID PRIMARY KEY             DEFAULT uuid_generate_v7(),
     name       VARCHAR(100)        NOT NULL,
     created_at TIMESTAMPTZ         NOT NULL DEFAULT NOW()
 );
